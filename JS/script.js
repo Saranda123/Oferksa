f54f7bc3da2990f77b585c0549f7a185d6b79ca0
Santiago Aranda <santiarandah7@gmail.com>
2025-07-09 08:21:18 -0500

    Refactor code structure and optimize performance across multiple modules

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const btnEnviar = document.querySelector('.buscar');
+btnEnviar.addEventListener('click', function(evento){
+    console.log(evento);
+    console.log('Buscando...');
+})
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+import { src, dest, watch} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css( done ){
+    src('src/scss/app.scss')
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( dest('build/css') )
+    done();
+}
+
+export function dev(){
+    watch('src/scss/app.scss', css)
+}
